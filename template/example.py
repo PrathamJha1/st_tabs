e1ecc07478846638c8037ac8c792924558acd05d
PrathamJha1 <prathamjha5683@gmail.com>
2023-09-06 19:38:55 +0530

    demo app for presentation deployed

--- template/example.py
+++ template/example.py
@@ -1,8 +1,16 @@
 import streamlit as st
 from st_tabs import TabBar
-component1=  TabBar(tabs=["Tab1","Tab2"],default=0,background = "black",color="grey",activeColor="blue",fontSize="20px")
-#Handle your tabbar usecases here.
-if(component1 == 0):
+
+component1 = TabBar(
+    tabs=["Tab1", "Tab2"],
+    default=0,
+    background="aliceblue",
+    color="black",
+    activeColor="#545fc3",
+    fontSize="20px",
+)
+# Handle your tabbar usecases here.
+if component1 == 0:
     st.write("Hooray! we are in Tab1")
 else:
     st.write("Yippee! We are in Tab2 ")
